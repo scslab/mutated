e30f91c89cf70dfe37d8a393580594fa05ca85ec
David Terei <code@davidterei.com>
2016-01-07 12:23:23 -0800

    Code formatting.

--- libserver/protocol.hh
+++ libserver/protocol.hh
@@ -2,16 +2,16 @@
 #define PROTOCOL_HH
 
 #define REQ_DELAY_SLEEP (1UL << 63)
-#define REQ_DELAY_MASK	(~(REQ_DELAY_SLEEP))
-#define REQ_MAX_DELAYS	16
+#define REQ_DELAY_MASK  (~(REQ_DELAY_SLEEP))
+#define REQ_MAX_DELAYS  16
 
 #include <cstdint>
 
 struct req_pkt {
-	int nr;		  /* the number of delays */
+	int nr;                          /* the number of delays */
 	int pad;
-	uint64_t tag;	  /* a unique indentifier for the request */
-	uint64_t delays[REQ_MAX_DELAYS];/* an array of delays */
+	uint64_t tag;                    /* a unique indentifier for the request */
+	uint64_t delays[REQ_MAX_DELAYS]; /* an array of delays */
 } __attribute__((packed));
 
 struct resp_pkt {
